4227a3e5b0465f9cf5e98649936a0e83adc20c47
Vladimir Eryshev <vladimir.eryshev@wukkerteam.com>
2017-01-12 23:30:54 +0300

    Initial Commit

---
+++ ControlsHomeTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ControlsHomeTask
+//
+//  Created by VLAD on 12/01/2017.
+//  Copyright © 2017 Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ControlsHomeTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControlsHomeTask
+//
+//  Created by VLAD on 12/01/2017.
+//  Copyright © 2017 Vlad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
